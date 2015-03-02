8ebff7f2c988904cc27161288461de6add1c9313
Alex Laidlaw <alex@2020productions>
2015-03-02 09:20:25 +0000

    First commit for trackstar test

---
+++ protected/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	 'connectionString' => 'mysql:host=127.0.0.1;dbname=trackstar',
+	 'emulatePrepare' => true,
+	 'username' => 'alex',
+	 'password' => 'testpass',
+	 'charset' => 'utf8',
+
+	
+);
